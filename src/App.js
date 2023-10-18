171c327c3cffda578071d017a86552f62dd6772e
Edgineer10 <redlilan15@gmail.com>
2023-10-18 11:50:06 +0900

    beforelunch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout';
-import Public from './components/Public';
-import Login from './features/auth/Login';
-import DashLayout
-  from './components/DashLayout';
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login'
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+
 function App() {
   return (
     <Routes>
@@ -11,7 +12,6 @@ function App() {
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
         <Route path='dash' element={<DashLayout />}>
-
         </Route>
       </Route>
     </Routes>
