bb6d4ee03893f77aaeaffde26f0df3a92b281b12
Edgineer10 <redlilan15@gmail.com>
2023-10-20 11:40:39 +0900

    10/20 lunch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
-import UsersList from "./features/users/UsersList";
+import UsersList from "./features/users/Userslist";
 import EditUser from "./features/users/EditUser";
 import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
