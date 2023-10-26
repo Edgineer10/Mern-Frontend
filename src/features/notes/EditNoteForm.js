116966d08c9bcc37fa6b92e6d9eb820df980c6c2
Edge Lilan <redlilan15@gmail.com>
2023-10-26 17:04:10 +0900

    10/26

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -1,83 +1,87 @@
-import { useState, useEffect } from "react"
-import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice"
-import { useNavigate } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
-import useAuth from "../../hooks/useAuth"
+import { useState, useEffect } from "react";
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "../../hooks/useAuth";
 
 const EditNoteForm = ({ note, users }) => {
+  const { isManager, isAdmin } = useAuth();
 
-  const { isManager, isAdmin } = useAuth()
+  const [updateNote, { isLoading, isSuccess, isError, error }] =
+    useUpdateNoteMutation();
 
-  const [updateNote, {
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  }] = useUpdateNoteMutation()
+  const [
+    deleteNote,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteNoteMutation();
 
-  const [deleteNote, {
-    isSuccess: isDelSuccess,
-    isError: isDelError,
-    error: delerror
-  }] = useDeleteNoteMutation()
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const [title, setTitle] = useState(note.title)
-  const [text, setText] = useState(note.text)
-  const [completed, setCompleted] = useState(note.completed)
-  const [userId, setUserId] = useState(note.user)
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
+  const [completed, setCompleted] = useState(note.completed);
+  const [userId, setUserId] = useState(note.user);
 
   useEffect(() => {
-
     if (isSuccess || isDelSuccess) {
-      setTitle('')
-      setText('')
-      setUserId('')
-      navigate('/dash/notes')
+      setTitle("");
+      setText("");
+      setUserId("");
+      navigate("/dash/notes");
     }
+  }, [isSuccess, isDelSuccess, navigate]);
 
-  }, [isSuccess, isDelSuccess, navigate])
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onTextChanged = (e) => setText(e.target.value);
+  const onCompletedChanged = (e) => setCompleted((prev) => !prev);
+  const onUserIdChanged = (e) => setUserId(e.target.value);
 
-  const onTitleChanged = e => setTitle(e.target.value)
-  const onTextChanged = e => setText(e.target.value)
-  const onCompletedChanged = e => setCompleted(prev => !prev)
-  const onUserIdChanged = e => setUserId(e.target.value)
-
-  const canSave = [title, text, userId].every(Boolean) && !isLoading
+  const canSave = [title, text, userId].every(Boolean) && !isLoading;
 
   const onSaveNoteClicked = async (e) => {
     if (canSave) {
-      await updateNote({ id: note.id, user: userId, title, text, completed })
+      await updateNote({ id: note.id, user: userId, title, text, completed });
     }
-  }
+  };
 
   const onDeleteNoteClicked = async () => {
-    await deleteNote({ id: note.id })
-  }
+    await deleteNote({ id: note.id });
+  };
 
-  const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
-  const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+  const created = new Date(note.createdAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+  const updated = new Date(note.updatedAt).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
 
-  const options = users.map(user => {
+  const options = users.map((user) => {
     return (
-      <option
-        key={user.id}
-        value={user.id}
+      <option key={user.id} value={user.id}>
+        {" "}
+        {user.username}
+      </option>
+    );
+  });
 
-      > {user.username}</option >
-    )
-  })
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
+  const validTitleClass = !title ? "form__input--incomplete" : "";
+  const validTextClass = !text ? "form__input--incomplete" : "";
 
-  const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
-  const validTitleClass = !title ? "form__input--incomplete" : ''
-  const validTextClass = !text ? "form__input--incomplete" : ''
+  const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
 
-  const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
-
-
-  let deleteButton = null
+  let deleteButton = null;
   if (isManager || isAdmin) {
     deleteButton = (
       <button
@@ -87,14 +91,14 @@ const EditNoteForm = ({ note, users }) => {
       >
         <FontAwesomeIcon icon={faTrashCan} />
       </button>
-    )
+    );
   }
 
   const content = (
     <>
       <p className={errClass}>{errContent}</p>
 
-      <form className="form" onSubmit={e => e.preventDefault()}>
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
         <div className="form__title-row">
           <h2>Edit Note #{note.ticket}</h2>
           <div className="form__action-buttons">
@@ -110,7 +114,8 @@ const EditNoteForm = ({ note, users }) => {
           </div>
         </div>
         <label className="form__label" htmlFor="note-title">
-          Title:</label>
+          Title:
+        </label>
         <input
           className={`form__input ${validTitleClass}`}
           id="note-title"
@@ -122,7 +127,8 @@ const EditNoteForm = ({ note, users }) => {
         />
 
         <label className="form__label" htmlFor="note-text">
-          Text:</label>
+          Text:
+        </label>
         <textarea
           className={`form__input form__input--text ${validTextClass}`}
           id="note-text"
@@ -132,7 +138,10 @@ const EditNoteForm = ({ note, users }) => {
         />
         <div className="form__row">
           <div className="form__divider">
-            <label className="form__label form__checkbox-container" htmlFor="note-completed">
+            <label
+              className="form__label form__checkbox-container"
+              htmlFor="note-completed"
+            >
               WORK COMPLETE:
               <input
                 className="form__checkbox"
@@ -144,8 +153,12 @@ const EditNoteForm = ({ note, users }) => {
               />
             </label>
 
-            <label className="form__label form__checkbox-container" htmlFor="note-username">
-              ASSIGNED TO:</label>
+            <label
+              className="form__label form__checkbox-container"
+              htmlFor="note-username"
+            >
+              ASSIGNED TO:
+            </label>
             <select
               id="note-username"
               name="username"
@@ -157,15 +170,23 @@ const EditNoteForm = ({ note, users }) => {
             </select>
           </div>
           <div className="form__divider">
-            <p className="form__created">Created:<br />{created}</p>
-            <p className="form__updated">Updated:<br />{updated}</p>
+            <p className="form__created">
+              Created:
+              <br />
+              {created}
+            </p>
+            <p className="form__updated">
+              Updated:
+              <br />
+              {updated}
+            </p>
           </div>
         </div>
       </form>
     </>
-  )
+  );
 
-  return content
-}
+  return content;
+};
 
-export default EditNoteForm
+export default EditNoteForm;
